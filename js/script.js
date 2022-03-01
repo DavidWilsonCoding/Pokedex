403b2edf6735f437bbe626ee79ece5afeb13d0da
David Wilson <david.wilson.coding@gmail.com>
2022-03-01 20:56:29 +0000

    Added Pokemon characters to pokemonList array

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-let pokemonList = [];
+let pokemonList = [
+
+    {
+        name: "Chameleon",
+        height: 1.1,
+        types: ["blaze", "solar power"]
+    },
+    {
+        name: "Wartortle",
+        height: 1,
+        types: ["rain-dish", "torrent"]
+    },
+    {
+        name: "Pidgeotto",
+        height: 1.1,
+        types: ["keen-eye", "tangled-feet", "big-pecks"]
+    }
+
+];
