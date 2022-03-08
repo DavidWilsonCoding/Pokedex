2fcc5ee961f97f0b05b508deb8c39d677e5640e3
David Wilson <david.wilson.coding@gmail.com>
2022-03-08 16:44:43 +0000

    Added loadList function inside URL to populate pokemonList through api fetch method

--- js/script.js
+++ js/script.js
@@ -3,49 +3,77 @@ let pokemonRepository = (function(){
     
     let pokemonList = [];
 
-        // Adds Pokemon to pokemonList
-        function add(pokemon) {
-            pokemonList.push(pokemon);
-        }
+    //declare variable to hold URL of Pokemon API 
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
-        // Returns all pokemon to pokemonRespoitory
-        function getAll() {
-            return pokemonList;
-        }
+    //add Pokemon to pokemonList
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
 
-        // Add an event listener which fires on a click event for pokemon buttons
-        function addListener(button, pokemon) {
-            button.addEventListener("click", function () {
-            showDetails(pokemon);
+    //return all pokemon to pokemonRespoitory
+    function getAll() {
+        return pokemonList;
+    }
+
+    //add an event listener which fires on a click event for pokemon buttons
+    function addListener(button, pokemon) {
+        button.addEventListener("click", function () {
+        showDetails(pokemon);
+        });
+    }
+
+    //creates a list of pokemon in HTML buttons and adds to the <ul> in document
+    function addListItem(pokemon) {
+        let pokemonUl = document.querySelector(".pokemon-list");
+        let listPokemon = document.createElement('li');
+        let button = document.createElement('button');
+        button.innerText = pokemon.name;
+        button.classList.add("pokemon-button");
+        listPokemon.appendChild(button);
+        pokemonUl.appendChild(listPokemon);
+        addListener(button, pokemon);
+    }
+
+    //loads pokemon from pokemon api
+    function loadList() {
+        //get list of pokemon from apiUrl
+        return fetch(apiUrl).then(function (response) {
+            //convert response to json objects
+            return response.json();
+        }).then(function (json) {
+            //forEach loop to convert json objects to js objects, adding  key value pairs for pokemon name and details URL 
+            json.results.forEach(function (item) {
+            let pokemon = {
+                name: item.name,
+                detailsUrl: item.url
+            };
+            //call add function to push pokemon to pokemonList
+            add(pokemon);
             });
-        }
+            //error handling
+        }).catch(function (e) {
+            console.error(e);
+        })
+    }
 
-        // Creates a list of pokemon in HTMl buttons and adds to the <ul> in document
-        function addListItem(pokemon) {
-            let pokemonUl = document.querySelector(".pokemon-list");
-            let listPokemon = document.createElement('li');
-            let button = document.createElement('button');
-            button.innerText = pokemon.name;
-            button.classList.add("pokemon-button");
-            listPokemon.appendChild(button);
-            pokemonUl.appendChild(listPokemon);
-            addListener(button, pokemon);
-        }
+    //logs pokemon name to the console
+    function showDetails(pokemon) {
+        console.log(pokemon.name);
+    }
 
-        //logs pokemon name to the console
-        function showDetails(pokemon) {
-            console.log(pokemon.name);
-        }
-
-        return {
-            add: add,
-            getAll: getAll,
-            addListItem: addListItem,
-            showDetails: showDetails
-        };
+    return {
+        add: add,
+        getAll: getAll,
+        addListItem: addListItem,
+        showDetails: showDetails,
+        loadList: loadList
+    };
     
-    })();
+})();
 
-pokemonRepository.getAll().forEach(function (pokemon) {
-    pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+    pokemonRepository.getAll().forEach(function (pokemon) {
+        pokemonRepository.addListItem(pokemon);
+    });
 });
